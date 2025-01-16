b46c69a7842e479688c975c1d7524b693254de61
Jesus Alberto <bethoronlive@DESKTOP-8H2VSGO>
2025-01-15 19:54:06 -0600

    feat(product, list): implement inputs and output communication
    Created inputs and an output in the reusable ProductComponent.
    Integrated its logic in the parent ListComponent to handle emitted events effectively.

--- store/src/app/domains/products/components/product/product.component.ts
+++ store/src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-product',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  @Input({required:true}) img: string = '';
+  @Input({required:true}) price: number = 0;
+  @Input({required:true}) title: string = '';
 
+  @Output() addToCart = new EventEmitter();
+  addToCartHandler(){
+    this.addToCart.emit('Este es un mensaje desde el hijo ' + this.title);
+  }
 }
--- store/src/app/domains/products/pages/list/list.component.ts
+++ store/src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from "../../components/product/product.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  addToCartFromChild(event:string){
+    console.log(`Estamos en el padre: ${event}`);
+  }
 }
